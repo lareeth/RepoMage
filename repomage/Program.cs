4bd108b0fcb0f5f285a32d1df24c5743685dd3ab
Gareth Luckett <gareth.luckett@gmail.com>
2019-01-17 23:38:43 +0000

    Add project files.

---
+++ repomage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repomage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
