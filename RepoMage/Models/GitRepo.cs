fbd8e56a554fb88c902585064d1c0e4dc54ccc6c
Gareth Luckett <gareth.luckett@gmail.com>
2019-01-30 23:31:26 +0000

    Added model for git repo. Added git provider interface.

---
+++ RepoMage/Models/GitRepo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RepoMage.Models
+{
+    public class GitRepo
+    {
+        public string Name { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
---
+++ RepoMage/Providers/IGitProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RepoMage.Models;
+
+namespace RepoMage.Providers
+{
+    public interface IGitProvider
+    {
+        IList<GitRepo> _repos { get; set; }
+
+        void Setup(GitAccount account);
+        IList<GitRepo> GetRepos();
+        IList<GitRepo> GetOldRepos();
+    }
+}
