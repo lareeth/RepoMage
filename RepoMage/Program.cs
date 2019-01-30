04b63e21eacfbc3aa95334ab50b0370853114495
Gareth Luckett <gareth.luckett@gmail.com>
2019-01-30 02:52:45 +0000

    Added initial DI

--- RepoMage/Program.cs
+++ RepoMage/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace RepoMage
 {
@@ -6,6 +10,15 @@ namespace RepoMage
     {
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json")
+                .Build();
+
+            IServiceProvider services = new ServiceCollection()
+                .AddOptions()
+                .BuildServiceProvider();
+
             Console.WriteLine("Hello World!");
 
             Console.ReadLine();
